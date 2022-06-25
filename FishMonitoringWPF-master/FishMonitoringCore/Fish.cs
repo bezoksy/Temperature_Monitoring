5055dff1d6825edf49e80185600e9a9b40358b3b
bezoksy <96592214+bezoksy@users.noreply.github.com>
2022-06-25 09:30:35 +0300

    Core without report

---
+++ FishMonitoringWPF-master/FishMonitoringCore/Fish.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishMonitoringCore
+{
+    public abstract class Fish
+    {
+        public string name;
+        public Quality quality;
+
+        public Fish(Quality q)
+        {
+            quality = q;
+        }
+        public abstract bool isValid();
+
+        public abstract string GetReport();
+
+    }
+
+    public class FrozenFish : Fish
+    {
+        public double maxStoreTemp;
+        public TimeSpan deathTime;
+
+        public FrozenFish(Quality q, double t, TimeSpan d) : base(q)
+        {
+            maxStoreTemp = t;
+            deathTime = d;
+        }
+
+        public override bool isValid()
+        {
+            return !((quality as TempQuality).GetTempUpperTime(maxStoreTemp) > deathTime);
+        }
+
+        public override string GetReport()
+        {
+            string report = "Fish is bad";
+            if (!isValid())
+            {
+                foreach (DateTime key in (quality as TempQuality).temperature.Keys)
+                {
+                    if ((quality as TempQuality).temperature[key] > maxStoreTemp)
+                    {
+                        report += "\n" + $"{key}\t{(quality as TempQuality).temperature[key]}";
+                    }
+                }
+            }
+            else
+            {
+                report = "All OK";
+            }
+
+            return report;
+        }
+    }
+}
---
+++ FishMonitoringWPF-master/FishMonitoringWPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FishMonitoringCore;
+
+namespace FishMonitoringWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReport_Click(object sender, RoutedEventArgs e)
+        {
+            int interval = 10;
+            string tempData = temperature.Text;
+
+
+        }
+    }
+}
