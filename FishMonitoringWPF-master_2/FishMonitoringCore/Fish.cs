f374958cce3bb50e0ab46c28e7c7e6165d22d0e2
bezoksy <96592214+bezoksy@users.noreply.github.com>
2022-06-25 10:04:01 +0300

    Creting a report

---
+++ FishMonitoringWPF-master_2/FishMonitoringCore/Fish.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishMonitoringCore
+{
+    public abstract class Fish
+    {
+        public string name;
+        public Quality quality;
+
+        public Fish(Quality q)
+        {
+            quality = q;
+        }
+        public abstract bool isValid();
+
+        public abstract string GetReport();
+
+    }
+
+    public class FrozenFish : Fish
+    {
+        public double maxStoreTemp;
+        public double minStoreTemp;
+        public TimeSpan deathTimeUpper;
+        public TimeSpan deathTimeLower;
+
+        public FrozenFish(Quality q, double maxTemp, double minTemp, TimeSpan dUpper, TimeSpan dLower) : base(q)
+        {
+            maxStoreTemp = maxTemp;
+            minStoreTemp = minTemp;
+            deathTimeUpper = dUpper;
+            deathTimeLower = dLower;
+        }
+
+        public override bool isValid()
+        {
+            return !((quality as TempQuality).GetTempUpperTime(maxStoreTemp) > deathTimeUpper || ((quality as TempQuality).GetTempLowerTime(minStoreTemp) > deathTimeLower));
+        }
+
+        public override string GetReport()
+        {
+            string report = "Fish is bad";
+            if (!isValid())
+            {
+                foreach (DateTime key in (quality as TempQuality).temperature.Keys)
+                {
+                    if ((quality as TempQuality).temperature[key] > maxStoreTemp || (quality as TempQuality).temperature[key] < minStoreTemp)
+                    {
+                        report += "\n" + $"{key}\t{(quality as TempQuality).temperature[key]}";
+                    }
+                }
+            }
+            else
+            {
+                report = "All OK";
+            }
+
+            return report;
+        }
+    }
+}
---
+++ FishMonitoringWPF-master_2/FishMonitoringWPF/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FishMonitoringCore;
+
+namespace FishMonitoringWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReport_Click(object sender, RoutedEventArgs e)
+        {
+            int interval = 10;
+            string tempData = temperature.Text;
+            DateTime time = Convert.ToDateTime(datetime.Text);
+
+            Quality quality = new TempQuality(interval, tempData, time);
+            double maxtemp = Convert.ToDouble(maxTemp.Text);
+            int maxtine = Convert.ToInt32(maxTIme.Text);
+            int mintime = Convert.ToInt32(minTime.Text);
+            double mintemp = Convert.ToDouble(minTemp.Text);
+            Fish fish = new FrozenFish(quality, maxtemp, mintemp, new TimeSpan(0, maxtine, 0), new TimeSpan(0, mintime, 0));
+            MessageBox.Show(fish.GetReport());
+        }
+    }
+}
